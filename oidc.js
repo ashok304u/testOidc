86d6292e67f20c4923f54344f9e36c1c68a8be68
ashok304u <130762667+ashok304u@users.noreply.github.com>
2023-04-20 18:12:59 +0530

    Update oidc.js

--- oidc.js
+++ oidc.js
@@ -1,3 +1,1 @@
-<input id=username type="text" placeholder="Github Username or Repo link">
-<button onclick="fetch(`https://api.github.com/users/${username.value.replace(/^.*com[/]([^/]*).*$/,'$1')}/events/public`).then(e=> e.json()).then(e => [...new Set([].concat.apply([],e.filter(x => x.type==='PushEvent').map(x => x.payload.commits.map(c => c.author.email)))).values()]).then(x => results.innerText = x)">GO, Get email!</button>
-<div id=results></div>
+
